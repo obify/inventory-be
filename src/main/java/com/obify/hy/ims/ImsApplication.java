bf22ee4da7cd18da5bcabf862935cf6adf8b3f71
Ranjan <ranjanpandey2006@gmail.com>
2024-11-01 15:11:47 +0100

    msg

---
+++ src/main/java/com/obify/hy/ims/ImsApplication.java
@@ -0,0 +1,16 @@
+package com.obify.hy.ims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+
+@SpringBootApplication
+public class ImsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsApplication.class, args);
+	}
+
+}
